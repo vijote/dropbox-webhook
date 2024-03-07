abda24415d5ea7afb60c2875ce6881f6c28527e8
Juan Ignacio Dominguez <38574701+vijote@users.noreply.github.com>
2024-03-07 16:37:43 -0300

    Update index.js

--- index.js
+++ index.js
@@ -27,39 +27,35 @@ app.get('/', (req, res) => {
 
 // Route to handle incoming webhook calls from Dropbox
 app.post('/dropbox/webhook', (req, res) => {
-  console.log(req.body);
-  console.log('req:', req);
-  // Make sure this is a valid request from Dropbox
-  const signature = req.headers['x-dropbox-signature'];
-  const hmacSignature = crypto.createHmac('sha256', process.env.APP_SECRET).update(JSON.stringify(req.body)).digest('hex');
-  console.log('signature:', signature);
-  console.log('hmacSignature:', hmacSignature);
-  if (signature !== hmacSignature) {
-    console.log("invalid signature!");
-    res.status(403).send('Invalid signature');
-    return;
+  try {
+    const { accounts } = req.body.list_folder;
+    accounts.forEach(() => {
+      console.log('TODO: Handle account changes');
+    });
+
+    console.log('success!');
+
+    // Respond with an empty body
+    res.status(200).send('');
+  } catch (error) {
+    console.log('error', error);
   }
-
-  // Process each changed user ID in a separate thread (in Node.js, we use asynchronous processing)
-  const accounts = req.body.list_folder.accounts;
-  accounts.forEach(account => {
-    processUser(account);
-  });
-
-  console.log("success!");
-  // Respond with an empty body
-  res.status(200).send('');
 });
 
 app.get('/dropbox/webhook', (req, res) => {
-  console.log('challenge received!');
-  // Get the challenge parameter from the query string
-  const challenge = req.query.challenge;
+  try {
+    console.log('query:', req.query);
 
-  // Set up the response with the challenge parameter
-  res.set('Content-Type', 'text/plain');
-  res.set('X-Content-Type-Options', 'nosniff');
-  res.send(challenge);
+    // Get the challenge parameter from the query string
+    const { challenge } = req.query;
+
+    // Set up the response with the challenge parameter
+    res.set('Content-Type', 'text/plain');
+    res.set('X-Content-Type-Options', 'nosniff');
+    res.send(challenge);
+  } catch (error) {
+    console.log('error:', error);
+  }
 });
 
 // Start the server
